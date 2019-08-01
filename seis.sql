8ed058e426a9ba42da112a5555afe336ea5cfff1
arweng <arweng6448@gmail.com>
2019-08-01 09:44:58 +0800

    db

--- seis.sql
+++ seis.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 25, 2019 at 04:27 PM
+-- Generation Time: Aug 01, 2019 at 09:44 AM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.18
 
@@ -19,6 +19,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `seis`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ack_reciept`
+--
+
+CREATE TABLE `ack_reciept` (
+  `id` int(11) NOT NULL,
+  `properrty_no` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `acquisition_date` date DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `inventory_tag` int(11) DEFAULT NULL,
+  `classification` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `_condition` varchar(255) DEFAULT NULL,
+  `acquisition` varchar(255) DEFAULT NULL,
+  `system_log` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,31 +70,10 @@ CREATE TABLE `equipment` (
 --
 
 INSERT INTO `equipment` (`id`, `property_no`, `description`, `model`, `acquisition`, `acquisition_date`, `acquisition_cost`, `_serial`, `inventory_tag`, `classification`, `_condition`, `supplier`, `remarks`, `system_log`) VALUES
-(1, '1234', 'Good As Fuck', 'dekugtar Aircon', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(2, '5678', 'Good As Fuck', 'dekugtar Unit', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(3, '1400778', 'Better', 'Cellphone', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(4, '1400778', 'dasdsa', 'asdas', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(5, 'dasdas', 'sadasd', 'dasdasd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(6, 'asdsaf', 'sdfsd', 'sdfsdf', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(7, 'sdfsd', 'fsdf', 'sdfsd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(8, 'fsdfsd', 'sdfsdf', 'gfdgdfg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(9, '456', 'Maba', 'Cellphone', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(10, '789', 'Ho', 'Cellphone', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(11, '1400776', 'fsdfsd', 'ljk', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(12, '1400773', 'gf', 'ghdfgsd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(13, '1400779', 'dhfjgh', 'fasd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(14, '1400780', 'Most', 'dsadsa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(15, '1400781', 'Oyweah', 'adasdsa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(16, 'sdfsdfsdf', 'fdgfdgfd', 'dfgdfg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(17, 'dasdsaf', 'asdadsd', 'asdasd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(18, 'asdasda', 'asdasda', 'sadasda', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(19, 'asdasd', 'sadasd', 'sadasd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(20, '1400778', 'Huawei', 'Cellphone', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(21, 'ssdf345', 'Asus', 'Tablet', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(22, '435rfdv', 'Macbook Pro', 'Laptop', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(23, '1400778', 'Huawei', 'Cellphone', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(24, 'ssdf345', 'Asus', 'Tablet', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44'),
-(25, '435rfdv', 'Macbook Pro', 'Laptop', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-07-25 06:55:44');
+(1, '34234', 'fdsdf', 'sdfs', 'sdfs', '2019-02-02', 787, '32', 0, 'asd', 'asd', 'asd', 'asd', '2019-07-31 03:52:54'),
+(2, '1400778', 'dfgdfg', 'dfgsd', 'sdfgsdf', '2019-07-30', 4334534, '456456fghfgh', 46456, 'fghfg', 'hfghfg', 'hfghf', 'ghdfghd', '2019-07-30 08:50:01'),
+(3, '2324', 'fdg', 'fdg', 'dfgfg', '2019-07-31', 25, 'sdr23456', 4532450, 'dfgdf', 'fdgfdg', 'fdgfd', 'gfdg', '2019-07-31 00:24:48'),
+(4, '09876', 'jhsada', 'snjabdjabs', 'sdfsadfs', '2019-08-01', 123456, '234sdfgh', 13242, 'dfsdf', 'sdfs', 'dfgsfdg', 'fdsgdf', '2019-08-01 00:14:50');
 
 -- --------------------------------------------------------
 
@@ -103,6 +104,12 @@ INSERT INTO `users_tbl` (`users_id`, `users_firstname`, `users_middlename`, `use
 --
 
 --
+-- Indexes for table `ack_reciept`
+--
+ALTER TABLE `ack_reciept`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `equipment`
 --
 ALTER TABLE `equipment`
@@ -119,10 +126,15 @@ ALTER TABLE `users_tbl`
 --
 
 --
+-- AUTO_INCREMENT for table `ack_reciept`
+--
+ALTER TABLE `ack_reciept`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `equipment`
 --
 ALTER TABLE `equipment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `users_tbl`
 --
